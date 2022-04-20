1fbb818b761bea7d2fb8a8b19a75df18a0eca211
Abdelaziz <abdel.msr@hotmail.com>
2022-04-20 14:07:42 +0200

    ajout exo 7

---
+++ day-1/exercise-7.js
@@ -0,0 +1,14 @@
+export const my_is_posi_neg =  (nbr)  => {
+	if ( nbr === null  || typeof nbr == "undefined" || nbr > 0)
+	{
+		return "POSITIF"
+	}
+	else if (nbr < 0)
+	{
+		return "NEGATIF";
+	}
+	else
+	{
+		return "NEUTRAL"
+	}
+}
